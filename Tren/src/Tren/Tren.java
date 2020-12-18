7f9f9ed1ae2a94bf99146e27948b5e8eac928a63
ufuk2606 <ufukozkar11@gmail.com>
2020-12-18 17:05:56 +0100

    proje bitti

--- Tren/src/Tren/Tren.java
+++ Tren/src/Tren/Tren.java
@@ -1,5 +1,31 @@
 package Tren;
 
-public class Tren {
+import Vagon.Vagon;
 
+import java.util.Scanner;
+
+public class Tren {
+    public static void main(String[] args) {
+        System.out.println("Lütfen 1 ile 10 arasinda bir tahminde bulununuz.");
+        Scanner vagonTahmini = new Scanner(System.in);
+        int tahmin = vagonTahmini.nextInt(10);
+        if (tahmin < 11 && tahmin > 0) {
+            for (int i = 1; i <= 4; i++) {
+                if (tahmin == Vagon.kacaginVagonu) {
+                    System.out.println("Tebrikler " + i + ". tahminizde buldunuz.");
+                    System.out.println(" Puaniniz " + 100);
+                    break;
+                } else if (Vagon.kacaginVagonu > tahmin) {
+                    System.out.println("Daha buyuk bir deger giriniz.");
+                } else {
+                    System.out.println("Daha kucuk bir deger giriniz.");
+                }
+                if (i != 4) {
+                    tahmin = vagonTahmini.nextInt(10);
+                } else {
+                    System.out.println("Hakkiniz bitti kacagi bulamadiniz . Puaniniz " + 0);
+                }
+            }
+        }
+    }
 }
---
+++ Tren/src/Vagon/Vagon.java
@@ -0,0 +1,5 @@
+package Vagon;
+
+public class Vagon {
+    public static int kacaginVagonu = (int) Math.floor(Math.random() * 10 + 1);
+}
