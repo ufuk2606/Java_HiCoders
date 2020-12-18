e808e00f040816fe499269b108eb32295463f7fe
ufuk2606 <ufukozkar11@gmail.com>
2020-12-18 18:19:02 +0100

    yorum satirlari eklendi

--- Tren/src/Tren/Tren.java
+++ Tren/src/Tren/Tren.java
@@ -1,28 +1,32 @@
 package Tren;
 
-import Vagon.Vagon;
+import Vagon.Vagon; // Vagon clasinda olusturulan sayiyi bu classta kullandigimiz icin import edilir
 
-import java.util.Scanner;
-
-public class Tren {
+import java.util.Scanner; // consoldan bilgl alirken kullandigimiz icin import edilir
+/*
+Bu program 1 ile 10 sayilari arasinda tahmin edilen bir sayisi 4 denemede bulma uzerine calisir.
+Kacinci tahminde bilindiyse ona gore puanlama yapar.
+4 seferde bulunmadiysa puani 0 verir ve programi sonlandirir.
+ */
+public class Tren {  // Tren class i olusturur.
     public static void main(String[] args) {
-        System.out.println("Lütfen 1 ile 10 arasinda bir tahminde bulununuz.");
-        Scanner vagonTahmini = new Scanner(System.in);
-        int tahmin = vagonTahmini.nextInt(10);
-        if (tahmin < 11 && tahmin > 0) {
-            for (int i = 100; i >= 25; i-=25) {
-                if (tahmin == Vagon.kacaginVagonu) {
-                    System.out.println("Tebrikler. Bildiniz.. Puaniniz : " + i );
+        System.out.println("Lütfen 1 ile 10 arasinda bir tahminde bulununuz.( Toplam 4 hakkiniz vardir.)"); // consola yazi yazdirir
+        Scanner vagonTahmini = new Scanner(System.in);  // consoldan bilgi ister
+        int tahmin = vagonTahmini.nextInt(10);  // girilen degeri int tipine cevirir
+        if (tahmin < 11 && tahmin > 0) {  // eger girilen deger istenilen aralikta degilse program calismaz
+            for (int i = 100; i >= 25; i-=25) { // 4 kez donguye girer
+                if (tahmin == Vagon.kacaginVagonu) { // girilen deger ile vagon classindaki rastgele olusturulan deger kiyaslanir
+                    System.out.println("Tebrikler. Bildiniz.. Puaniniz : " + i ); // degerler birbiriyle ayniysa consola yazi ve puani basar
                     break;
-                } else if (Vagon.kacaginVagonu > tahmin) {
-                    System.out.println("Malesef arka vagonlarda. ");
-                } else {
-                    System.out.println("Malesef on vagonlarda. ");
-                }
-                if (i != 25) {
+                } else if ( i != 25 && Vagon.kacaginVagonu > tahmin) {
+                    System.out.println("Malesef arka vagonlarda. Bir tahmin daha.. ");// girilen degere gore yonlendirme yapar ve bir deger daha girmesini ister
                     tahmin = vagonTahmini.nextInt(10);
-                } else {
-                    System.out.println("Malesef bulamadiniz. Hakkiniz bitti. Puaniniz : " + 0);
+                } else if (i != 25 && Vagon.kacaginVagonu < tahmin){
+                    System.out.println("Malesef on vagonlarda. Bir tahmin daha.. ");  // girilen degere gore yonlendirme yapar ve bir deger daha girmesini ister
+                    tahmin = vagonTahmini.nextInt(10);
+                }
+                else {
+                    System.out.println("Malesef bulamadiniz. Hakkiniz bitti. Puaniniz : " + 0); // tahmin sayisi bitince bilgl verir ve programi bitirir
                 }
             }
         }
--- Tren/src/Vagon/Vagon.java
+++ Tren/src/Vagon/Vagon.java
@@ -1,5 +1,7 @@
 package Vagon;
-
+/*
+ Bu class 1 ile 10 arasinda rastgele bir numara olusturur
+ */
 public class Vagon {
     public static int kacaginVagonu = (int) Math.floor(Math.random() * 10 + 1);
 }
